b15229fee0ca1740fbf85c97bbad56da1b530899
Anksh S <ankitsi031999@gmail.com>
2023-09-07 23:43:10 +0530

    Add ping url and views

--- Weather_Report/urls.py
+++ Weather_Report/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from metar_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('metar/ping/', views.ping, name='ping'),
+  
 ]
--- metar_app/views.py
+++ metar_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import requests
+
 
 # Create your views here.
+
+
+
+import requests
+
+def ping(request):
+    return JsonResponse({'data': 'pong'})
